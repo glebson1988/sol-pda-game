7b43c07b55785237f4f64104ede81b256d4981a8
glebson1988 <glebson1988@yandex.ru>
2025-01-12 19:23:30 +0100

    Test

--- tests/sol-pda-game.ts
+++ tests/sol-pda-game.ts
@@ -1,16 +1,43 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { SolPdaGame } from "../target/types/sol_pda_game";
+import { expect } from "chai";
 
 describe("sol-pda-game", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolPdaGame as Program<SolPdaGame>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Sets and changes name", async () => {
+    const [userStatsPDA] = await PublicKey.findProgramAddress(
+      [Buffer.from("user-stats"), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+      .createUserStats("john")
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc();
+
+    expect(
+      (await program.account.userStats.fetch(userStatsPDA)).name
+    ).to.equal("john");
+
+    await program.methods
+      .changeUserName("jane")
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc();
+
+    expect(
+      (await program.account.userStats.fetch(userStatsPDA)).name
+    ).to.equal("jane");
   });
 });
