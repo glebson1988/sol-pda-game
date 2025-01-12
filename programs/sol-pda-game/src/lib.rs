445d6fad0846a9c60f96bc04b0b83ff092b4930e
glebson1988 <glebson1988@yandex.ru>
2025-01-12 17:07:48 +0100

    Implement PDA-based UserStats creation and name change functionality

--- programs/sol-pda-game/src/lib.rs
+++ programs/sol-pda-game/src/lib.rs
@@ -5,12 +5,59 @@ declare_id!("8zqoMqZLPjdjGxsTbcKBEtJDQn9wJB1Sh4Ubiu1jHYCW");
 #[program]
 pub mod sol_pda_game {
     use super::*;
+    // handler function
+    pub fn create_user_stats(ctx: Context<CreateUserStats>, name: String) -> Result<()> {
+        let user_stats: &mut _ = &mut ctx.accounts.user_stats;
+        user_stats.level = 0;
+        if name.as_bytes().len() > 200 {
+            //proper error handling omitted for brevity
+            panic!();
+        }
+        user_stats.name = name;
+        user_stats.bump = ctx.bumps.user_stats;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn change_user_name(ctx: Context<ChangeUserName>, new_name: String) -> Result<()> {
+        if new_name.as_bytes().len() > 200 {
+            panic!();
+        }
+        ctx.accounts.user_stats.name = new_name;
         Ok(())
     }
 }
 
+#[account]
+pub struct UserStats {
+    level: u16,
+    name: String,
+    bump: u8,
+}
+
+// validation struct
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUserStats<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // space: 8 discriminator + 2 level + 4 name length + 200 name + 1 bump
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 2 + 4 + 200 + 1,
+        seeds = [b"user-stats", user.key().as_ref()],
+        bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ChangeUserName<'info> {
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"user-stats", user.key().as_ref()],
+        bump = user_stats.bump
+    )]
+    pub user_stats: Account<'info, UserStats>
+}
